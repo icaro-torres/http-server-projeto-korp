2e09223b4dc94947a0b31b750a0713899f1d8e2c
Icaro Torres <98storresm@gmail.com>
2026-07-07 11:20:17 -0300

    feat: Aplicação para requisões HTTP em GO

---
+++ app/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+)
+
+type ResponseBody struct {
+	Nome    string `json:"nome"`
+	Horario string `json:"horario`
+}
+
+func projetoKorpHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	currentTime := time.Now().UTC().Format(time.RFC3339)
+
+	response := ResponseBody{
+		Nome:    "Projeto Korp",
+		Horario: currentTime,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	http.HandleFunc("/projeto-korp", projetoKorpHandler)
+
+	fmt.Println("Servidor HTTP rodando na porte 8080...")
+	http.ListenAndServe(":8080", nil)
+}
